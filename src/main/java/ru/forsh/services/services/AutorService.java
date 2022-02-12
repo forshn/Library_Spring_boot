8bf85a63413c9963d749d1da97735f5efe60b9e1
Nikolay <forsh_nikolay@mail.ru>
2022-02-12 17:19:42 +0300

    add saveAll,save methods

--- src/main/java/ru/forsh/services/services/AutorService.java
+++ src/main/java/ru/forsh/services/services/AutorService.java
@@ -1,9 +1,23 @@
 package ru.forsh.services.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.forsh.services.entites.AutorEntity;
+import ru.forsh.services.entites.BookEntity;
 import ru.forsh.services.repositories.AutorRepository;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AutorService {
-    AutorRepository repository;
+    private final AutorRepository repository;
+
+    public List<AutorEntity> getAll(){
+        return repository.findAll();
+    }
+
+    public void saveAll(List<AutorEntity> autorEntities){
+        repository.saveAll(autorEntities);
+    }
 }
