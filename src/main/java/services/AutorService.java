6a89a8f781182d49315274adb725be64438d985f
Nikolay <forsh_nikolay@mail.ru>
2022-02-12 16:48:40 +0300

    Repositories add, services add

---
+++ src/main/java/services/AutorService.java
@@ -0,0 +1,9 @@
+package services;
+
+import org.springframework.stereotype.Service;
+import repositories.AutorRepository;
+
+@Service
+public class AutorService {
+    AutorRepository repository;
+}
---
+++ src/main/java/services/BookService.java
@@ -0,0 +1,9 @@
+package services;
+
+import org.springframework.stereotype.Service;
+import repositories.BookRepository;
+
+@Service
+public class BookService {
+    BookRepository repository;
+}
