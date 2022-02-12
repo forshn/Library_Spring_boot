f14a01f7f0ff1ea0b2d158de913bc3e69f20e34e
Nikolay <forsh_nikolay@mail.ru>
2022-02-12 16:39:53 +0300

    project initialization, entities add

---
+++ src/main/java/entites/AutorEntity.java
@@ -0,0 +1,26 @@
+package entites;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Accessors(chain = true)
+@Entity
+@Data
+@Table(name = "autor_table")
+public class AutorEntity {
+
+    @Id
+    @Column(name = "id_autor")
+    @GeneratedValue(generator = "generator")
+    @GenericGenerator(name = "generator", strategy = "increment")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstNameAutor;
+
+    @Column(name = "last_name")
+    private String lastNameAutor;
+}
---
+++ src/main/java/entites/BookEntity.java
@@ -0,0 +1,31 @@
+package entites;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Accessors(chain = true)
+@Entity
+@Data
+@Table(name = "book_table")
+public class BookEntity {
+
+    @Id
+    @Column(name = "id_book")
+    @GenericGenerator(name = "generator", strategy = "increment")
+    @GeneratedValue(generator = "generator")
+    private Integer id;
+
+    @Column(name = "book_name")
+    private String nameBook;
+
+    @Column(name = "creation_date")
+    private Integer yearCreat;
+
+    @Column(name = "autor_id")
+    private Integer autorId;
+
+
+}
